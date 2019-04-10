d35cdd2bf11653492dcac182cdc3d05a117e36d3
Jasmine Lawrence <lawrence.jasmine@gmail.com>
2019-04-09 23:11:27 -0700

    add desciption for whale talk

--- JavaScript/whaletalk.js
+++ JavaScript/whaletalk.js
@@ -1,3 +1,14 @@
+/*
+Project from Codecademy Javascript Course
+
+There are a few simple rules for translating text to whale language:
+
+- There are no consonants. Only vowels excluding “y”.
+- The u‘s and e‘s are extra long, so we must double them in our program.
+
+Once we have converted text to the whale language, the result is sung slowly, as is a custom in the ocean.
+*/
+
 let input = 'Teach me how you whale talk';
 const vowels = ['a','e','i','o','u'];
 
